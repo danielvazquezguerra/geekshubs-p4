c41da920ff78aa9c2f1d0407826e56ac23da4ddd
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-03-23 06:04:44 +0100

    bulkCreate en peliculas funcionando

--- models/Pelis.js
+++ models/Pelis.js
@@ -32,6 +32,30 @@ Pelis.sync({force: true})
 
            })
         })
+        .then( ()=>{
+          
+            Pelis.create ({
+  
+               titulo: "IT: Chapter Two",
+  
+               descripcion: "Han pasado casi 30 años desde que el 'El club de los perdedores', formado por Bill, Beverly, Richie, Ben, Eddie, Mike y Stanley, se enfrentaran al macabro y despiadado Pennywise (Bill Skarsgård). En cuanto tuvieron oportunidad, abandonaron el pueblo de Derry, en el estado de Maine, que tantos problemas les había ocasionado. Sin embargo, ahora, siendo adultos, parece que no pueden escapar de su pasado. Todos deberán enfrentarse de nuevo al temible payaso para descubrir si de verdad están preparados para superar sus traumas de la infancia.",
+  
+              year: "2019",
+  
+             })
+          })
+          .then( ()=>{
+          
+            Pelis.create ({
+  
+               titulo: "Star Wars: El ascenso de skywalker",
+  
+               descripcion: "La Resistencia sobreviviente se enfrenta a la Primera Orden una vez más mientras continúa el viaje de Rey, Finn y Poe Dameron. Con el poder y el conocimiento de las generaciones detrás de ellos, comienza la batalla final.",
+  
+              year: "2020",
+  
+             })
+          })
            
 
 module.exports = Pelis;
